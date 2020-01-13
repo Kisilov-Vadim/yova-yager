1f0e92a3f3dfdfb1a0d021738e078ab217168800
Vadim Kisilov <djvadya16@gmail.com>
2020-01-13 12:23:21 +0200

    Updates

---
+++ precache-manifest.ccbdf75004322dff585c059df8a68a56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be77b30c4c53e18bdff98bdf82513b24",
+    "url": "/yova-yager/index.html"
+  },
+  {
+    "revision": "43c6f10191214a80c3e2",
+    "url": "/yova-yager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b44f4d334c0cc508612f",
+    "url": "/yova-yager/static/js/2.d81e8aef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yova-yager/static/js/2.d81e8aef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43c6f10191214a80c3e2",
+    "url": "/yova-yager/static/js/main.67767c0c.chunk.js"
+  },
+  {
+    "revision": "b1a4c8c547c012532984",
+    "url": "/yova-yager/static/js/runtime-main.e5584b23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yova-yager/static/media/logo.5d5d9eef.svg"
+  }
+]);
