526cf4ac15f946ab2b27a281a1c29e465c8d7dfc
VadimK <djvadya16@gmail.com>
2020-03-16 11:20:21 +0200

    first correct test

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import {shallow} from 'enzyme'
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 import Header from '../components/Header/Header'
+
+configure({adapter: new Adapter()});
 
 describe('Header component', () => {
 
   it('main .header container render', () => {
-    let component = shallow(<Header />)
+    const component = shallow(<Header />);
     let headerClass = component.find('.header')
     expect(headerClass.length).toBe(1)
   })
